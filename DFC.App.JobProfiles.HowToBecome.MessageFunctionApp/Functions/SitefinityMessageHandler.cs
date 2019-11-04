76d8de31ade9192669571d6b51995192506971a4
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2019-11-04 10:20:39 +0000

    Updated function app to log AlreadyReported
    Updated function app to log AlreadyReported

--- DFC.App.JobProfiles.HowToBecome.MessageFunctionApp/Functions/SitefinityMessageHandler.cs
+++ DFC.App.JobProfiles.HowToBecome.MessageFunctionApp/Functions/SitefinityMessageHandler.cs
@@ -62,7 +62,11 @@ namespace DFC.App.JobProfiles.HowToBecome.MessageFunctionApp.Functions
                 case HttpStatusCode.Created:
                     log.LogInformation($"{ClassFullName}: JobProfile Id: {messageContentId}: Created segment");
                     break;
-
+                    
+                case HttpStatusCode.AlreadyReported:
+                   log.LogInformation($"{ClassFullName}: JobProfile Id: {messageContentId}: Segment previously updated");
+                   break;
+                    
                 default:
                     log.LogWarning($"{ClassFullName}: JobProfile Id: {messageContentId}: Segment not Posted: Status: {result}");
                     break;
