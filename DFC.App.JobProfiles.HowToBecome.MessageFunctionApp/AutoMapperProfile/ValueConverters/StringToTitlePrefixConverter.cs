a70e840dce26ca8c0531c90700895dada5dc2014
Ian Crisp <icrisp@program-systems.co.uk>
2019-11-19 12:14:35 +0000

    Implemented a value converter for string to TitlePrefix enum

---
+++ DFC.App.JobProfiles.HowToBecome.MessageFunctionApp/AutoMapperProfile/ValueConverters/StringToTitlePrefixConverter.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DFC.App.JobProfiles.HowToBecome.Data.Enums;
+using System;
+
+namespace DFC.App.JobProfiles.HowToBecome.MessageFunctionApp.AutoMapperProfile.ValueConverters
+{
+    public class StringToTitlePrefixConverter : IValueConverter<string, TitlePrefix>
+    {
+        public TitlePrefix Convert(string sourceMember, ResolutionContext context)
+        {
+            if (Enum.TryParse<TitlePrefix>(sourceMember, out var result))
+            {
+                return result;
+            }
+
+            return TitlePrefix.AsDefined;
+        }
+    }
+}
