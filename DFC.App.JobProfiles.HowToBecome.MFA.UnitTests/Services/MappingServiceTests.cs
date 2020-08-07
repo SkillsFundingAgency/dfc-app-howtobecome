a7e26b0232a02b15e23eb782fc1afcfc13312388
gurney <Gurney@as-tech.co.uk>
2020-08-07 08:19:52 +0100

    Completed unit tests

--- DFC.App.JobProfiles.HowToBecome.MFA.UnitTests/Services/MappingServiceTests.cs
+++ DFC.App.JobProfiles.HowToBecome.MFA.UnitTests/Services/MappingServiceTests.cs
@@ -99,6 +99,27 @@ namespace DFC.App.JobProfiles.HowToBecome.MFA.UnitTests.Services
             actualMappedModel.Data.Title.Should().Be(expectedTitle);
         }
 
+        [Theory]
+        [InlineData("Title", "No Prefix", "title")]
+        [InlineData("Title", "Prefix with a", "a title")]
+        [InlineData("Title", "Prefix with an", "an title")]
+        [InlineData("Title", "No Title", "")]
+        [InlineData("noVowel", "No Defined", "a novowel")]
+        [InlineData("aVowel", "No Defined", "an avowel")]
+        public void HtbTitleIsPreFixedCorrectly(string title, string prefix, string expectedTitle)
+        {
+            //Arrange
+            var jobProfileMessage = new JobProfileMessage() { DynamicTitlePrefix = prefix, Title = title };
+            var message = JsonConvert.SerializeObject(jobProfileMessage);
+
+            //Act
+            var actualMappedModel = mappingService.MapToSegmentModel(message, SequenceNumber);
+
+            //Asserts
+            actualMappedModel.Data.Title.Should().Be(expectedTitle);
+        }
+
+
         private static JobProfileMessage BuildJobProfileMessage()
         {
             return new JobProfileMessage
