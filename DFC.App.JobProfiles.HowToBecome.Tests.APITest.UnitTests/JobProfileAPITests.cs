291169cfeac66c507df7912c6cea2e355b7d354e
James Barnes <james@testtrends.co.uk>
2020-08-25 14:11:33 +0100

    Story/DFC-13000: Enhancements (#97)
    * Framework enhancements
    
    * One more test class to update. All tests passing.
    
    * Test framework refactored
    
    * Added test frameworks sub folder to unit test directory
    
    * Added new xunit test project
    
    * Added first unit test
    
    * Added 2 unit tests.

---
+++ DFC.App.JobProfiles.HowToBecome.Tests.APITest.UnitTests/JobProfileAPITests.cs
@@ -0,0 +1,36 @@
+﻿using DFC.Api.JobProfiles.IntegrationTests.Model.Support;
+using DFC.Api.JobProfiles.IntegrationTests.Support.API;
+using DFC.App.JobProfiles.HowToBecome.Tests.API.IntegrationTests.Model.APIResponse;
+using DFC.App.JobProfiles.HowToBecome.Tests.API.IntegrationTests.Support.API;
+using DFC.App.JobProfiles.HowToBecome.Tests.API.IntegrationTests.Support.API.RestFactory.Interfaces;
+using FakeItEasy;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DFC.App.JobProfiles.HowToBecome.Tests.APITest.UnitTests
+{
+    public class JobProfileAPITests
+    {
+        [Fact]
+        public async Task JobProfileAPICallsOnRestClientExecute()
+        {
+            // Arrange
+            var fakeRestClient = A.Fake<IRestClient>();
+            var fakeRestRequest = A.Fake<IRestRequest>();
+            var fakeRestClientFactory = A.Fake<IRestClientFactory>();
+            var fakeRestRequestFactory = A.Fake<IRestRequestFactory>();
+            A.CallTo(() => fakeRestClientFactory.Create(A<Uri>.Ignored)).Returns(fakeRestClient);
+            A.CallTo(() => fakeRestRequestFactory.Create(A<string>.Ignored)).Returns(fakeRestRequest);
+            var apiSettings = new APISettings() { Endpoint = A.Fake<Uri>() };
+            var jobProfileApi = new JobProfileApi(fakeRestClientFactory, fakeRestRequestFactory, new AppSettings(), apiSettings);
+
+            // Act
+            await jobProfileApi.GetById<HowToBecomeAPIResponse>("fakeValue").ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => fakeRestClient.Execute<HowToBecomeAPIResponse>(fakeRestRequest)).MustHaveHappenedOnceExactly();
+        }
+    }
+}
