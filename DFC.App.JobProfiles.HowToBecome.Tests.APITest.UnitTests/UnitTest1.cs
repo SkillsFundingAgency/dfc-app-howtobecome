8c6a0f848d2e4f6cf126796b4e15104ab64840dd
James Barnes <james@testtrends.co.uk>
2020-08-25 12:06:14 +0100

    Added new xunit test project

---
+++ DFC.App.JobProfiles.HowToBecome.Tests.APITest.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DFC.App.JobProfiles.HowToBecome.Tests.APITest.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
