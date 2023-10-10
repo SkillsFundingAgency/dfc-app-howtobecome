51e68a0413b90770ffb4f45f4a6928c6adeda821
Lea Hayes <lea.hayes@education.gov.uk>
2023-10-10 12:54:35 +0100

    Initial iteration of the real story feature.

--- DFC.App.JobProfiles.HowToBecome/ApiModels/HowToBecomeApiModel.cs
+++ DFC.App.JobProfiles.HowToBecome/ApiModels/HowToBecomeApiModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using DFC.App.JobProfiles.HowToBecome.Data.Models.DataModels;
+using System.Collections.Generic;
 
 namespace DFC.App.JobProfiles.HowToBecome.ApiModels
 {
@@ -9,5 +10,13 @@ namespace DFC.App.JobProfiles.HowToBecome.ApiModels
         public EntryRoutesApiModel EntryRoutes { get; set; }
 
         public MoreInformationApiModel MoreInformation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the social proof real story when one has been selected for the job profile.
+        /// </summary>
+        /// <value>
+        /// A <see cref="DataModels.RealStory"/> when present; otherwise, a value of <c>null</c>.
+        /// </value>
+        public RealStory RealStory { get; set; }
     }
 }
