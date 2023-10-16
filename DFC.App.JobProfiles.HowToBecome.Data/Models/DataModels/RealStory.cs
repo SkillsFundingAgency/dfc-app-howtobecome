20e453417d7daed3cc0f87746967988bc4421b1a
Lea Hayes <lea.hayes@education.gov.uk>
2023-10-16 14:57:36 +0100

    Amend real story to include optional content from a `Further information` field.

--- DFC.App.JobProfiles.HowToBecome.Data/Models/DataModels/RealStory.cs
+++ DFC.App.JobProfiles.HowToBecome.Data/Models/DataModels/RealStory.cs
@@ -8,6 +8,14 @@
         public string Title { get; set; }
 
         /// <summary>
+        /// Gets or sets the summary text for of the real story.
+        /// </summary>
+        /// <value>
+        /// Plain text.
+        /// </value>
+        public string Summary { get; set; }
+
+        /// <summary>
         /// Gets or sets the thumbnail of the real story.
         /// </summary>
         /// <value>
@@ -16,12 +24,12 @@
         public Thumbnail Thumbnail { get; set; }
 
         /// <summary>
-        /// Gets or sets the summary text for of the real story.
+        /// Gets or sets the further information HTML content that is shown below the thumbnail.
         /// </summary>
-        /// <value>
-        /// Plain text.
-        /// </value>
-        public string Summary { get; set; }
+        /// <remarks>
+        /// <para>This is raw HTML text that is input into a WYSIWYG field in the CMS.</para>
+        /// </remarks>
+        public string FurtherInformationHtml { get; set; }
 
         /// <summary>
         /// Gets or sets the body HTML content of the real story.
